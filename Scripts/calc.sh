80f608bbee15a8e87874ad2545884344334d0aee
nvegas1230 <vboxuser@thehostname.testdomain.local>
2026-04-07 10:12:54 -0500

    Finished the calculator script

--- Scripts/calc.sh
+++ Scripts/calc.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-num_one=""
-num_two=""
-operator='$name'
+num_one=''
+num_two=''
+operator=''
+answer=''
 
 menu_options() {
 	echo -e "
+	first number = $num_one
+	second number = $num_two
+	operator = $operator
+	previous answer = $answer
+
 	|Choose an option from below|
 	-----------------------------
 	|
@@ -21,6 +27,16 @@ menu_options() {
 	|
 	-----------------------------
 	"
+	read input
+	echo
+
+	case $input in
+		'a'|'A') set_first_number;;
+		'b'|'B') set_second_number;;
+		'c'|'C') set_operator;;
+		'd'|'D') calculate;;
+		'x'|'X') exit;;
+	esac
 }
 
 set_first_number() {
@@ -43,3 +59,18 @@ set_operator() {
 		*) echo "You did not select a valid operator; skipping operator selection.";;
 	esac
 }
+
+calculate() {
+	if [[ -z $num_one || -z $num_two || -z $operator ]]; then
+		return
+	fi
+
+	answer=$(($num_one $operator $num_two))
+	echo "The answer to $num_one $operator $num_two is $answer"
+}
+
+test=true
+
+while $test; do
+	menu_options
+done
